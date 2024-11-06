8f868e3b76e9d5a4c75e7bdf1bbea152e3ec8f83
Leonardo <srleon19@hotmail.com>
2024-11-06 18:00:30 -0300

    remove route params

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,11 +1,10 @@
 import { EmailTemplate } from '@/app/Components/Email-template';
 import { Resend } from 'resend';
-import { NextApiRequest, NextApiResponse } from 'next';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL || '';
 
-export async function POST(_: NextApiRequest, res: NextApiResponse) {
+export async function POST() {
   try {
     const { data, error } = await resend.emails.send({
       from: fromEmail,
